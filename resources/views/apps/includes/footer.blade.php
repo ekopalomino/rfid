82feac000562d8958528bf8b1fef428381aca2b9
Eko Heru Wibowo <ekoheruwibowo@Ekos-MacBook-Air.local>
2024-02-17 11:40:49 +0700

    Update

--- resources/views/apps/includes/footer.blade.php
+++ resources/views/apps/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="page-footer">
-    <div class="page-footer-inner"> 2023 &copy; iTeOS Tech | PT. Agrinesia Raya
+    <div class="page-footer-inner"> 2023 &copy; PT. Agrinesia Raya
     </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
