57be445df4ca9eb7cb1e6f2cd7b89406a8aed5df
Eko Heru Wibowo <ekoheruwibowo@Ekos-MacBook-Air.local>
2024-05-21 22:04:26 +0700

    Update

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -27,7 +27,7 @@ class ProductExport implements FromCollection, WithHeadings, WithMapping, Should
             $product->name,
             $product->categories->name,
             $product->branches->name,
-            $product->locations->id,
+            $product->locations->location_name,
             $product->price,
             $product->specification,
             Carbon::parse($product->purchase_date)->toFormattedDateString(),
